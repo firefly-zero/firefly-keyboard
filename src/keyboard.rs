c49b394bc3b45ceada935364e5a3fa429c21d176
gram <git@orsinium.dev>
2026-03-12 13:28:22 +0100

    decouple update for pad and buttons

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -77,125 +77,127 @@ impl Keyboard {
 
     /// Updates keyboard input.
     pub fn update(&mut self) -> State {
-        let mut ret_state = State::Open;
-
         if !self.is_open {
             return State::Closed;
         }
 
-        let buttons = read_buttons(self.peer);
-        let pressed = buttons.just_pressed(&self.last_buttons);
-
         let pad = read_pad(self.peer);
-
         if let Some(pad) = pad {
-            let dpad = pad.as_dpad8();
-            let pressed = dpad.just_pressed(&self.last_pad.unwrap_or_default().as_dpad8());
-
-            let last_row_len =
-                self.board.rows.get(self.ysel as usize).unwrap().keys.len() as i32 - 1;
-
-            let mut xchg: i32 = 0;
-            let mut ychg: i32 = 0;
-
-            if pressed.up {
-                ychg -= 1;
-            } else if pressed.down {
-                ychg += 1;
-            }
-
-            if pressed.right {
-                xchg += 1;
-            } else if pressed.left {
-                xchg -= 1;
-            }
-
-            if (self.ysel as i32) + ychg > self.board.rows.len() as i32 - 1 {
-                self.ysel = 0;
-            } else if (self.ysel as i32) + ychg < 0 {
-                self.ysel = self.board.rows.len() as u32 - 1;
-            } else {
-                self.ysel += ychg as u32;
-            }
-
-            let row_len = self.board.rows.get(self.ysel as usize).unwrap().keys.len() as i32 - 1;
-
-            if last_row_len > row_len {
-                let mut cells = Vec::with_capacity(row_len as usize);
-
-                for (idx, keys) in self
-                    .board
-                    .rows
-                    .get(self.ysel as usize)
-                    .unwrap()
-                    .keys
-                    .iter()
-                    .enumerate()
-                {
-                    for _ in 0..keys.cells {
-                        cells.push(idx as u32);
-                    }
-                }
-
-                self.xsel = *cells.get(self.xsel as usize).unwrap();
-            } else if last_row_len < row_len {
-                // TODO: better logic
-
-                self.xsel += 1;
-            }
-
-            if (self.xsel as i32) + xchg > row_len {
-                self.xsel = 0;
-            } else if (self.xsel as i32) + xchg < 0 {
-                self.xsel = row_len as u32;
-            } else {
-                self.xsel += xchg as u32;
-            }
+            self.handle_pad(pad);
         }
 
+        let buttons = read_buttons(self.peer);
+        let pressed = buttons.just_pressed(&self.last_buttons);
+        let state = self.handle_buttons(pressed);
+        if matches!(state, State::JustCancelled | State::JustClosed(_)) {
+            self.is_open = false
+        }
+
+        self.last_pad = pad;
+        self.last_buttons = buttons;
+        state
+    }
+
+    fn handle_pad(&mut self, pad: Pad) {
+        let dpad = pad.as_dpad8();
+        let pressed = dpad.just_pressed(&self.last_pad.unwrap_or_default().as_dpad8());
+
+        let last_row_len = self.board.rows.get(self.ysel as usize).unwrap().keys.len() as i32 - 1;
+
+        let mut xchg: i32 = 0;
+        let mut ychg: i32 = 0;
+
+        if pressed.up {
+            ychg -= 1;
+        } else if pressed.down {
+            ychg += 1;
+        }
+
+        if pressed.right {
+            xchg += 1;
+        } else if pressed.left {
+            xchg -= 1;
+        }
+
+        if (self.ysel as i32) + ychg > self.board.rows.len() as i32 - 1 {
+            self.ysel = 0;
+        } else if (self.ysel as i32) + ychg < 0 {
+            self.ysel = self.board.rows.len() as u32 - 1;
+        } else {
+            self.ysel += ychg as u32;
+        }
+
+        let row_len = self.board.rows.get(self.ysel as usize).unwrap().keys.len() as i32 - 1;
+
+        if last_row_len > row_len {
+            let mut cells = Vec::with_capacity(row_len as usize);
+
+            for (idx, keys) in self
+                .board
+                .rows
+                .get(self.ysel as usize)
+                .unwrap()
+                .keys
+                .iter()
+                .enumerate()
+            {
+                for _ in 0..keys.cells {
+                    cells.push(idx as u32);
+                }
+            }
+
+            self.xsel = *cells.get(self.xsel as usize).unwrap();
+        } else if last_row_len < row_len {
+            // TODO: better logic
+
+            self.xsel += 1;
+        }
+
+        if (self.xsel as i32) + xchg > row_len {
+            self.xsel = 0;
+        } else if (self.xsel as i32) + xchg < 0 {
+            self.xsel = row_len as u32;
+        } else {
+            self.xsel += xchg as u32;
+        }
+    }
+
+    fn handle_buttons(&mut self, pressed: Buttons) -> State {
+        let mut state = State::Open;
         if pressed.e {
             if let Some(key) = self.board.get(self.xsel as usize, self.ysel as usize) {
                 match key.key_type {
                     KeyType::Char(c) => {
                         self.text.push(c);
-                        ret_state = State::TextChanged(self.text.clone());
+                        state = State::TextChanged(self.text.clone());
                     }
                     KeyType::Space => {
                         self.text.push(' ');
-                        ret_state = State::TextChanged(self.text.clone());
+                        state = State::TextChanged(self.text.clone());
                     }
                     KeyType::Backspace => {
                         self.text.pop();
-                        ret_state = State::TextChanged(self.text.clone());
+                        state = State::TextChanged(self.text.clone());
                     }
                     KeyType::Shift => self.board.shifted = !self.board.shifted,
                     KeyType::Ok => {
-                        ret_state = State::JustClosed(self.text.clone());
+                        state = State::JustClosed(self.text.clone());
                     }
                     KeyType::Cancel => {
-                        ret_state = State::JustCancelled;
+                        state = State::JustCancelled;
                     }
                 }
             }
         } else if pressed.w {
             if self.text.is_empty() {
                 // NOTE: remove?
-                ret_state = State::JustCancelled
+                state = State::JustCancelled
             } else {
                 self.text.pop();
-                ret_state = State::TextChanged(self.text.clone())
+                state = State::TextChanged(self.text.clone())
             }
         }
-
-        self.last_pad = pad;
-        self.last_buttons = buttons;
-
-        match ret_state {
-            State::JustCancelled | State::JustClosed(_) => self.is_open = false,
-            _ => {}
-        }
-
-        ret_state
+        state
     }
 
     /// Renders the keyboard.
@@ -216,17 +218,6 @@ impl Keyboard {
     /// Returns if the keyboard is currently open.
     pub fn is_open(&mut self) -> bool {
         self.is_open
-    }
-
-    /// Clears the keyboard's text.
-    pub fn clear(&mut self) {
-        self.text.clear();
-    }
-
-    /// Sets the keyboard's text.
-    pub fn set_text(&mut self, text: &str) {
-        self.text.clear();
-        self.text.push_str(text);
     }
 }
 
